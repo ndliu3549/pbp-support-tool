848df685852b8d9ef2e6291ea99b8900c02d24d8
Jimmy <ndliu3549@gmail.com>
2018-10-19 19:45:12 +0800

    fix bug

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
-	<div id="app">
-		<div style="text-align:center;">
+	<div id='app'>
+		<div style='text-align:center;'>
 			<h2>CPBL PLAY BY PLAY Support Tool</h2>
-			<button class="btn btn-primary" type="button" @click="BallSubmitMessage(listdata)">Import LOG data（JSON format）</button>
-			<button class="btn btn-primary" type="button" @click="CleanBallSubmitMessage()">Clean data</button>
+			<button class='btn btn-primary' type='button' @click='BallSubmitMessage(listdata)'>Import LOG data（JSON format）</button>
+			<button class='btn btn-primary' type='button' @click='CleanBallSubmitMessage()'>Clean data</button>
 		</div>
-		<ul id="ul">
+		<ul id='ul'>
 		</ul>
 	</div>
 </template>
 
 <script>
 	var config = {
-		apiKey: "AIzaSyCaWqHus5lbszeGpNqjh9PhgnKbTY_GpaM",
-		authDomain: "new-firebase-3065e.firebaseapp.com",
-		databaseURL: "https://new-firebase-3065e.firebaseio.com",
-		projectId: "new-firebase-3065e",
-		storageBucket: "new-firebase-3065e.appspot.com",
+		apiKey: 'AIzaSyCaWqHus5lbszeGpNqjh9PhgnKbTY_GpaM',
+		authDomain: 'new-firebase-3065e.firebaseapp.com',
+		databaseURL: 'https://new-firebase-3065e.firebaseio.com',
+		projectId: 'new-firebase-3065e',
+		storageBucket: 'new-firebase-3065e.appspot.com',
 	};
 	firebase.initializeApp(config);
 
@@ -96,19 +96,26 @@
 				let list = '';
 
 				$.each(val, function(i, item) {
-					list = `${list}<li>第${item.Game}場：${item.Team}：${item.Inning}：${item.Log}</li>
-					<input disabled id="input_Player" size="3" value=${item.Player} />
-					<input disabled id="input_Base1" size="3" value=${item.Base1} />
-					<input disabled id="input_Base2" size="3" value=${item.Base2} />
-					<input disabled id="input_Base3" size="3" value=${item.Base3} />
-					<input disabled id="input_Id" size="3" value=${item.Id} />
-					<input disabled id="input_Direction" size="1" value=${item.Direction} />
-					<input disabled id="input_Out" size="3" value=${item.Out} />
-					<input disabled id="input_Result" size="16" value=${item.Result} />
-					<button type="button" class="enable" data-key="${i}">啟用更正</button>
-					<button disabled id="summit_modify" type="button" class="edit"
-					data-key="${i}">送出更正</button>`
-
+					
+					list = `
+					
+					${list}
+					
+					<li>第${item.Game}場：${item.Team}：${item.Inning}：${item.Log}</li>
+					
+					<input disabled id='input_Player${i}' size='3' value=${item.Player} />
+					<input disabled id='input_Base1${i}' size='3' value=${item.Base1} />
+					<input disabled id='input_Base2${i}' size='3' value=${item.Base2} />
+					<input disabled id='input_Base3${i}' size='3' value=${item.Base3} />
+					<input disabled id='input_Id${i}' size='3' value=${item.Id} />
+					<input disabled id='input_Direction${i}' size='1' value=${item.Direction} />
+					<input disabled id='input_Out${i}' size='3' value=${item.Out} />
+					<input disabled id='input_Result${i}' size='16' value=${item.Result} />
+					
+					<button id='enable_modify${i}' type='button' class='enable' data-key=${i}>啟用更正</button>
+					<button disabled id='summit_modify${i}' type='button' class='summit' data-key=${i}>送出更正</button>
+					
+					`
 				});
 
 				$('ul').html(list)
@@ -116,49 +123,47 @@
 
 			$('#ul').on('click', '.enable', function() {
 
-				$("#input_Player").attr("disabled", false);
-				$("#input_Base1").attr("disabled", false);
-				$("#input_Base2").attr("disabled", false);
-				$("#input_Base3").attr("disabled", false);
-				$("#input_Id").attr("disabled", false);
-				$("#input_Direction").attr("disabled", false);
-				$("#input_Out").attr("disabled", false);
-				$("#input_Result").attr("disabled", false);
-				$("#summit_modify").attr("disabled", false);
+				var key = $(this).data('key');
+
+				$('#input_Player' + key).attr('disabled', false);
+				$('#input_Base1' + key).attr('disabled', false);
+				$('#input_Base2' + key).attr('disabled', false);
+				$('#input_Base3' + key).attr('disabled', false);
+				$('#input_Id' + key).attr('disabled', false);
+				$('#input_Direction' + key).attr('disabled', false);
+				$('#input_Out' + key).attr('disabled', false);
+				$('#input_Result' + key).attr('disabled', false);
+				$('#summit_modify' + key).attr('disabled', false);
+				
+				$('#enable_modify' + key).attr('disabled', true);
 			});
 
-			$('#ul').on('click', '.edit', function() {
+			$('#ul').on('click', '.summit', function() {
 
-				var update_player = $("#input_Player").val();
-				var update_base1 = $("#input_Base1").val();
-				var update_base2 = $("#input_Base2").val();
-				var update_base3 = $("#input_Base3").val();
-				var update_id = $("#input_Id").val();
-				var update_direction = $("#input_Direction").val();
-				var update_out = $("#input_Out").val();
-				var update_result = $("#input_Result").val();
 				var key = $(this).data('key');
 
 				BaseballRef.child(key).update({
-					"Player": update_player,
-					"Base1": update_base1,
-					"Base2": update_base2,
-					"Base3": update_base3,
-					"Id": update_id,
-					"Direction": update_direction,
-					"Out": update_out,
-					"Result": update_result
+					'Player': $('#input_Player' + key).val(),
+					'Base1': $('#input_Base1' + key).val(),
+					'Base2': $('#input_Base2' + key).val(),
+					'Base3': $('#input_Base3' + key).val(),
+					'Id': $('#input_Id' + key).val(),
+					'Direction': $('#input_Direction' + key).val(),
+					'Out': $('#input_Out' + key).val(),
+					'Result': $('#input_Result' + key).val()
 				});
 
-				$("#input_Player").attr("disabled", true);
-				$("#input_Base1").attr("disabled", true);
-				$("#input_Base2").attr("disabled", true);
-				$("#input_Base3").attr("disabled", true);
-				$("#input_Id").attr("disabled", true);
-				$("#input_Direction").attr("disabled", true);
-				$("#input_Out").attr("disabled", true);
-				$("#input_Result").attr("disabled", true);
-				$("#summit_modify").attr("disabled", true);
+				$('#input_Player' + key).attr('disabled', true);
+				$('#input_Base1' + key).attr('disabled', true);
+				$('#input_Base2' + key).attr('disabled', true);
+				$('#input_Base3' + key).attr('disabled', true);
+				$('#input_Id' + key).attr('disabled', true);
+				$('#input_Direction' + key).attr('disabled', true);
+				$('#input_Out' + key).attr('disabled', true);
+				$('#input_Result' + key).attr('disabled', true);
+				$('#summit_modify' + key).attr('disabled', true);
+				
+				$('#enable_modify' + key).attr('disabled', false);
 			});
 		}
 	}
