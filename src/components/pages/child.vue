6d649f327689c006ed120ff66011ce9e6417be15
Jimmy <ndliu3549@gmail.com>
2019-05-14 03:48:08 +0800

    rechange database

--- src/components/pages/child.vue
+++ src/components/pages/child.vue
@@ -14,11 +14,11 @@
 
 <script>
 	var config = {
-		apiKey: 'AIzaSyCaWqHus5lbszeGpNqjh9PhgnKbTY_GpaM',
-		authDomain: 'new-firebase-3065e.firebaseapp.com',
-		databaseURL: 'https://new-firebase-3065e.firebaseio.com',
-		projectId: 'new-firebase-3065e',
-		storageBucket: 'new-firebase-3065e.appspot.com',
+		apiKey: 'AIzaSyDuscttLRI74uotEQG_al8kKhU8QxKzgDI',
+		authDomain: 'new-firebase-8b1fc.firebaseapp.com',
+		databaseURL: 'https://new-firebase-8b1fc.firebaseio.com',
+		projectId: 'new-firebase-8b1fc',
+		storageBucket: 'new-firebase-8b1fc.appspot.com',
 	};
 	var BaseballRef = firebase.database().ref('Baseball');
 	var idArray = Array();
